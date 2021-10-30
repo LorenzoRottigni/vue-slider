84d99b73051713ea12354cf078879f1b1708bcd9
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-30 15:31:44 +0200

    Esercizio completo

--- scripts/app.js
+++ scripts/app.js
@@ -7,36 +7,78 @@ window.addEventListener('DOMContentLoaded',()=>{
         //root selector
         el: '#vue-root',
         //app variables
-        data:
-            slides = [
+        data: {
+            slides: [
                 {
                     src: '../images/01.jpg',
                     title: 'Svezia',
-                    text: 'Lorem1'
+                    text: 'Lorem Ispum Dolorem1'
                 },
                 {
                     src: '../images/02.jpg',
                     title: 'Svizzera',
-                    text: 'Lorem2'
+                    text: 'Lorem Ispum Dolorem2'
                 },
                 {
                     src: '../images/03.jpg',
                     title: 'Gran Bretagna',
-                    text: 'Lorem3'
+                    text: 'Lorem Ispum Dolorem3'
                 },
                 {
                     src: '../images/04.jpg',
                     title: 'Germania',
-                    text: 'Lorem4'
+                    text: 'Lorem Ispum Dolorem4'
                 },
                 {
                     src: '../images/05.jpg',
                     title: 'Paradise',
-                    text: 'Lorem5'
+                    text: 'Lorem Ispum Dolorem5'
+                },
+                {
+                    src: '../images/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem Ispum Dolorem1'
+                },
+                {
+                    src: '../images/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem Ispum Dolorem2'
+                },
+                {
+                    src: '../images/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem Ispum Dolorem3'
+                },
+                {
+                    src: '../images/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem Ispum Dolorem4'
+                },
+                {
+                    src: '../images/05.jpg',
+                    title: 'Paradise',
+                    text: 'Lorem Ispum Dolorem5'
                 }
-            ]
-            
-       
+            ],
+            activeIndex : 0
+        },
+        methods : {
+            setMainImage(newIndex){
+                this.activeIndex = newIndex
+            },
+            indexIncrementer(operation){
+                if(operation>0){
+                    if(this.activeIndex === this.slides.length)
+                        this.activeIndex = 0
+                    
+                    this.activeIndex++
+                }else{
+                    if(this.activeIndex === 0)
+                        this.activeIndex = this.slides.length
+                
+                    this.activeIndex--
+                }
+            }
         }
-    )
+    })
 })
