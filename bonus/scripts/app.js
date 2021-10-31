75e756dc612e79ef32bc6d35b4be8229e2cb107e
LorenzoRottigni <lorenzo@rottigni.net>
2021-10-31 15:57:23 +0100

    Js comments

--- bonus/scripts/app.js
+++ bonus/scripts/app.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded',()=>{
         el: '#vue-root',
         //app variables
         data: {
+            //slides object array
             slides: [
                 {
                     src: '../images/01.jpg',
@@ -60,15 +61,25 @@ window.addEventListener('DOMContentLoaded',()=>{
                     text: 'Lorem Ispum Dolorem5'
                 }
             ],
+            //index of the displayed image
             activeIndex : 0,
+            //mouse hover on main main
             mouseOnContainer : false,
+            //autoplayer clock
             clockTime : "3000",
+            //autoplayer loop handler
             sliderLoop: undefined
         },
         methods : {
+            /**
+             * @param {int} newIndex - The new index to set.
+             */
             setMainImage(newIndex){
                 this.activeIndex = newIndex
             },
+            /**
+             * @param {boolean} operation - (operation) ? index increment : index decrement
+             */
             indexIncrementer(operation){
                 if(operation){
                     if(this.activeIndex === ((this.slides.length)-1))
@@ -83,43 +94,66 @@ window.addEventListener('DOMContentLoaded',()=>{
                 }
                 
             },
+            /**
+             * @param {boolean} isOn - (isOn) ? mouse is on container : mouse is out container
+             */
             cursorHandler(isOn){
                 if(isOn)
                     this.mouseOnContainer = true
                 else
                     this.mouseOnContainer = false
             },
-            //on scroll slide click wait 3 extra seconds
-            scrollDelay(delay){
+            /**
+             * @description reset current loop time
+             */
+            scrollDelay(){
                 this.cursorHandler(true)
-                setTimeout(() => this.cursorHandler(false), parseInt(delay))
+                setTimeout(() => this.cursorHandler(false), parseInt(this.clockTime))
             },
+            /**
+             * @description (left-arrow || down-arrow) ? decrement and reset loop time : increment and reset loop time
+             */
             keyboardHandler(){
                 if (event.keyCode == 37 || event.keyCode == 38) {
                     this.indexIncrementer(false)
-                    this.scrollDelay(this.clockTime)
+                    this.scrollDelay()
                 } else if (event.keyCode == 39 || event.keyCode == 40 || event.keyCode == 32) {
                     this.indexIncrementer(true)
-                    this.scrollDelay(this.clockTime)
+                    this.scrollDelay()
                 }
             },
+            /**
+             * @description (checkbox.checked) ? set mouse out container : set mouse in container
+             */
             checkboxHandler(){
                 if(event.target.checked)
                     this.mouseOnContainer = false
                 else
                     this.mouseOnContainer = true
             },
+            /**
+             * @description start loop if mouse is not on container,
+             * looptime: this.clockTime
+             */
             startClock(){
                 this.sliderLoop = setInterval(()=> {
                     if(!this.mouseOnContainer)
-                        this.indexIncrementer(1)
+                        this.indexIncrementer(true)
                 }, parseInt(this.clockTime))  
             },
+            /**
+             * @description start loop with new clock time,
+             * looptime: this.clockTime
+             */
             restartClock(){
                 clearInterval(this.sliderLoop);
                 this.startClock()
             }
         },
+        /**
+         * @description executed at Vue loaded,
+         * starts loop, and sets keyup listener
+         */
         mounted(){
             this.startClock()
             document.addEventListener("keyup", this.keyboardHandler);
